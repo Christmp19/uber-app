f9f93b46f8c46585ba66478f29dc539318908241
Christie MPASSI <christmp19@gmail.com>
2024-06-20 12:16:06 +0100

    feat: Adding React Native Elements & React Native Navigation

--- App.js
+++ App.js
@@ -4,24 +4,44 @@ import './nativewind.config';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   console.log('Store initialized:', store.getState());
   return (
-    // <Provider store={store}>
-    //   <NavigationContainer>
-    //     <Stack.Navigator>
-    //       <Stack.Screen name="Home" component={HomeScreen} />
-    //     </Stack.Navigator>
-    //   </NavigationContainer>
-    // </Provider>
-
     <Provider store={store}>
-          <HomeScreen />
+      <NavigationContainer>
+    <SafeAreaProvider>
+        <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{
+                headerShown: false,
+
+              }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+        </Stack.Navigator>
+    </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
+
+    // <Provider store={store}>
+    //   <SafeAreaProvider>
+    //     <HomeScreen />
+    //   </SafeAreaProvider>
+    // </Provider>
   );
 }
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,5 +1,7 @@
-import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native'
+import { View, Text, FlatList, TouchableOpacity, Image, StyleSheet } from 'react-native'
 import React from 'react'
+import { Icon } from '@rneui/base';
+import { useNavigation } from '@react-navigation/native';
 
 const data = [
     {
@@ -18,24 +20,47 @@ const data = [
 
 
 const NavOptions = () => {
+    const navigation = useNavigation();
+
   return (
       <FlatList
           data={data}
           keyExtractor={(item) => item.id}
           horizontal
           renderItem={({ item }) => (
-              < TouchableOpacity className='p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40'>
+              < TouchableOpacity
+                  onPress={() => navigation.navigate(item.screen)}
+                  className='p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40'>
                   <View>
                       <Image
                           style={{ width: 120, height: 120, resizeMode: 'contain' }}
                           source={{ uri: item.image }}
                       />
                       <Text className='mt-2 text-lg font-semibold'>{item.title}</Text>
+                      <Icon
+                          style={styles.icon}
+                          name='arrowright'
+                          color='white'
+                          type='antdesign'
+                      />
                  </View>
               </TouchableOpacity>
           )}
       />
   )
 }
+
+const styles = StyleSheet.create({
+
+    icon: {
+        padding: 8,
+        backgroundColor: 'black',
+        borderRadius: 50,
+        width: 40,
+        height: 40,
+        marginTop: 16,
+    },
+});
+
 
 export default NavOptions
