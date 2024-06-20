d395c1e1547f43247c6b9d353213d9b38762697d
Christie MPASSI <christmp19@gmail.com>
2024-06-20 13:33:47 +0100

    feat: Adding Google Autocomplete Library

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,16 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel"],
+    plugins: [
+      "nativewind/babel",
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+        },
+      ],
+    ],
   };
 };
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,58 @@
 import { Image, SafeAreaView, Text, View } from 'react-native'
 import React from 'react'
 import NavOptions from '../components/NavOptions'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env'
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '../slices/navSlice';
+
 
 const HomeScreen = () => {
+    const dispatch = useDispatch();
+
     return (
         <SafeAreaView className="bg-white h-full ">
             <View className="p-5">
                 <Image
                     style={{
-                        width: 100, height: 100, resizeMode: 'contain'
+                        width: 100,
+                        height: 100,
+                        resizeMode: 'contain'
                     }}
                     source={{
                         uri: 'https://links.papareact.com/gzs',
                     }}
+                />
+
+                <GooglePlacesAutocomplete
+                    placeholder='Where from?'
+                    styles={{
+                        container: {
+                            flex: 0,
+                        },
+                        textInput: {
+                            fontSize: 18
+                        }
+                    }}
+                    onPress={(data, details=null) => { 
+                        dispatch(
+                            setOrigin({
+                            location: details.geometry.location,
+                            description: data.description
+                        }))
+
+                        dispatch(setDestination(null))
+                    }}
+                    fetchDetails={true}
+                    returnKeyType={"search"}
+                    enablePoweredByContainer={false}
+                    minLength={2}
+                    query={{
+                        key: GOOGLE_MAPS_APIKEY,
+                        language: 'en',
+                    }}
+                    nearbyPlacesAPI="GooglePlacesSearch"
+                    debounce={400}
                 />
                 <NavOptions />
             </View>
